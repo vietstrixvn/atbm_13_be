d97db56f6596edc79d98447a5134340cff81806a
protam113 <hoangpm2003.strix@gmail.com>
2025-06-10 09:43:16 +0700

    update

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -1,6 +1,6 @@
-import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-import { ConfigService } from '@nestjs/config';
+import { Injectable, NestMiddleware, Logger } from "@nestjs/common";
+import { Request, Response, NextFunction } from "express";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
@@ -11,27 +11,30 @@ export class CorsMiddleware implements NestMiddleware {
     this.logger.log(`Incoming request from origin: ${req.headers.origin}`); // 👈 thêm dòng này
 
     const allowedOrigins = this.configService
-      .get<string>('ALLOWED_ORIGINS')
-      ?.split(',') || ['http://localhost:3000'];
+      .get<string>("ALLOWED_ORIGINS")
+      ?.split(",") || [
+      "http://localhost:3000",
+      "https://atbm-fb-clone-12.onrender.com",
+    ];
     const origin = req.headers.origin;
 
     if (origin && allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
+      res.setHeader("Access-Control-Allow-Origin", origin);
       this.logger.log(`CORS enabled for origin: ${origin}`);
     }
 
     res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET,HEAD,PUT,PATCH,POST,DELETE',
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE"
     );
     res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Content-Type, Accept, Authorization, X-API-KEY, api-key',
+      "Access-Control-Allow-Headers",
+      "Content-Type, Accept, Authorization, X-API-KEY, api-key"
     );
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
 
-    if (req.method === 'OPTIONS') {
-      this.logger.log('CORS preflight request handled');
+    if (req.method === "OPTIONS") {
+      this.logger.log("CORS preflight request handled");
       res.sendStatus(200);
       return;
     }
